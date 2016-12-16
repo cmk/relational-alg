3f0fe970de52cd5e2aa53fb7a634af77cf0f236a
chris <chris@datascience.com>
2016-12-16 13:55:05 -0800

    added more expressions

--- src/Select/Expression.hs
+++ src/Select/Expression.hs
@@ -9,7 +9,7 @@ module Select.Expression
   , As
   ) where
 
--- AST for SQL expressions
+-- Untyped AST for SQL expressions
 data Expression variable
   = LiteralBool Bool
   | LiteralInt Int
@@ -20,6 +20,17 @@ data Expression variable
   | And (Expression variable) (Expression variable)
   | Or  (Expression variable) (Expression variable)
   | Equ (Expression variable) (Expression variable)
+  | Neq (Expression variable) (Expression variable)
+  | Gt  (Expression variable) (Expression variable)
+  | Gte (Expression variable) (Expression variable)
+  | Lt  (Expression variable) (Expression variable)
+  | Lte (Expression variable) (Expression variable)
+  | Neg (Expression variable)
+  | Add (Expression variable) (Expression variable)
+  | Sub (Expression variable) (Expression variable)
+  | Mul (Expression variable) (Expression variable)
+  | Div (Expression variable) (Expression variable)
+  | Mod (Expression variable) (Expression variable)
   deriving
     ( Read
     , Show
