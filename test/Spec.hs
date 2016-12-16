6a141f26b03508c182c15c26fb8a6f4be68835d6
chris <chris@datascience.com>
2016-12-16 14:15:11 -0800

    added orders.csv, customers.csv and join test in Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -15,10 +15,31 @@ main = hspec $
       execute selectTable "output_people.csv"
       "output_people.csv" `shouldHaveSameContentAs` "data/people.csv"
 
+    it "performs filtering and comparisons correctly" $ do
+      execute selectName "output_name.csv"
+      "output_name.csv" `shouldHaveSameContentAs` "data/expected_output_name.csv"
+
+    it "performs joins correctly" $ do
+      execute selectJoin "output_join.csv"
+      "output_join.csv" `shouldHaveSameContentAs` "data/expected_output_join.csv"
 
 selectTable :: SelectIdentifier
 selectTable = SELECT (TABLE "data/people.csv")
 
+selectName :: SelectIdentifier
+selectName =
+  SELECT $ [Column "first_name" `AS` "name"]
+    `FROM` TABLE "data/people.csv" `WHERE` (Column "age" `Gte` LiteralInt 40)
+
+selectJoin :: SelectIdentifier
+selectJoin = SELECT $
+  [ Column "orders.order_id" `AS` "order_id"
+  , Column "customers.customer_name" `AS` "customer_name"
+  ] `FROM`
+  ( TABLE "data/orders.csv" `AS` "orders"
+    `INNER_JOIN_ON`
+    TABLE "data/customers.csv" `AS` "customers"
+  ) (Column ("orders","customer_id") `Equ` Column ("customers","customer_id"))
 
 shouldHaveSameContentAs :: FilePath -> FilePath -> Expectation
 file1 `shouldHaveSameContentAs` file2 =
