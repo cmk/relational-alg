4fe4b3a16f8fd4e1156f40542ef756cb30a638d0
chris <chris@datascience.com>
2016-12-16 14:04:00 -0800

    added people.csv and hspec test

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,5 +7,23 @@ import System.IO
 import Test.Hspec
 
 main :: IO ()
-main = undefined
+main = hspec $
 
+  describe "execute" $ do
+
+    it "can select a table" $ do
+      execute selectTable "output_people.csv"
+      "output_people.csv" `shouldHaveSameContentAs` "data/people.csv"
+
+
+selectTable :: SelectIdentifier
+selectTable = SELECT (TABLE "data/people.csv")
+
+
+shouldHaveSameContentAs :: FilePath -> FilePath -> Expectation
+file1 `shouldHaveSameContentAs` file2 =
+  withFile file1 ReadMode $ \handle1 ->
+    withFile file2 ReadMode $ \handle2 -> do
+      contents1 <- hGetContents handle1
+      contents2 <- hGetContents handle2
+      contents1 `shouldBe` contents2
