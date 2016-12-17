2a17e3c15c5a6c230d256d95df43da14798a879a
chris <chris@datascience.com>
2016-12-16 18:06:40 -0800

    fixed cabal file

--- src/Select.hs
+++ src/Select.hs
@@ -6,6 +6,9 @@ module Select
 
 import Select.Relation
 import Select.Expression
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+import Data.Csv
 
 -- Top level `Select` AST
 newtype Select scope variable table
@@ -20,4 +23,14 @@ execute
   :: SelectIdentifier
   -> FilePath -- output
   -> IO ()
-execute selectStatement output = undefined
+execute (SELECT (TABLE input)) output = putStr "hi"
+
+
+-- evaluate (SumNode op left right) = 
+--     let lft = evaluate left
+--         rgt = evaluate right
+--     in
+--         case op of
+--           Plus  -> lft + rgt
+--           Minus -> lft - rgt
+-- selectTable = SELECT (TABLE "data/people.csv")
