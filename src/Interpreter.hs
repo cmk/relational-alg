3cd0f14d6ad76a9e35b7c9e219b936a692236f9c
chris <chris@datascience.com>
2016-12-18 17:08:09 -0800

    rolling Evaluator back to: type Evaluator a = State SymTab a

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,54 +1,33 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
+
 module Interpreter where
 
 import Data.Char
 import qualified Data.Map as M
 import Control.Monad.State
-import Control.Monad (liftM, ap, guard)
-import Control.Monad.Trans.Maybe
 
 import Select.Expression
 import Select.Relation
 import Table
 
 type SymTab = M.Map String Value
-type Evaluator a = MaybeT (State SymTab) a
+type Evaluator a = State SymTab a
 
 
-checkNum :: (Value,Value) -> Bool
-checkNum e = 
-  case e of
-    (IntValue l, IntValue r) -> True
-    (RealValue l, RealValue r) -> True
-    _ -> False
-
-checkInt :: (Value,Value) -> Bool
-checkInt e = 
-  case e of
-    (IntValue l, IntValue r) -> True
-    _ -> False
-
-checkBool :: (Value,Value) -> Bool
-checkBool e = 
-  case e of
-    (BoolValue l, BoolValue r) -> True
-    _ -> False
-    
-evaluateE :: Expression a -> Evaluator Value
+evaluateE :: Show a =>  Expression a -> Evaluator Value
       
 evaluateE (LiteralInt x) = return $ IntValue x
 evaluateE (LiteralReal x) = return $ RealValue x
 
--- evaluateE (Column x) =  do
---     symTab <- get
---     guard $ M.member x symTab
---     case M.lookup x symTab of
---       Just v -> return v
+evaluateE (Column str) = do
+  symTab <- get
+  case M.lookup (show str) symTab of
+    Just v -> return v
+    Nothing -> error $ "Undefined variable"
 
 evaluateE (Add left right) = do
     lft <- evaluateE left
     rgt <- evaluateE right
-    guard $ checkNum (lft,rgt)
     case (lft,rgt) of
       (IntValue l, IntValue r) -> return $ IntValue (l + r)
       (RealValue l, RealValue r) -> return $ RealValue (l + r)
@@ -56,7 +35,6 @@ evaluateE (Add left right) = do
 evaluateE (Sub left right) = do
     lft <- evaluateE left
     rgt <- evaluateE right
-    guard $ checkNum (lft,rgt)
     case (lft,rgt) of
       (IntValue l, IntValue r) -> return $ IntValue (l - r)
       (RealValue l, RealValue r) -> return $ RealValue (l - r)
@@ -64,7 +42,6 @@ evaluateE (Sub left right) = do
 evaluateE (Mul left right) = do
     lft <- evaluateE left
     rgt <- evaluateE right
-    guard $ checkNum (lft,rgt)
     case (lft,rgt) of
       (IntValue l, IntValue r) -> return $ IntValue (l * r)
       (RealValue l, RealValue r) -> return $ RealValue (l * r)
@@ -72,14 +49,12 @@ evaluateE (Mul left right) = do
 evaluateE (Div left right) = do
     lft <- evaluateE left
     rgt <- evaluateE right
-    guard $ checkInt (lft,rgt)
     case (lft,rgt) of
       (IntValue l, IntValue r) -> return $ IntValue (l `div` r)
 
 evaluateE (Mod left right) = do
     lft <- evaluateE left
     rgt <- evaluateE right
-    guard $ checkInt (lft,rgt)
     case (lft,rgt) of
       (IntValue l, IntValue r) -> return $ IntValue (l `mod` r)
       
